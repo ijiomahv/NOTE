557a7c8ae4d173fdd814133c40eb650a42281f0d
unknown <ijiomahv@gmail.com>
2025-03-07 23:11:29 +0100

    styling

--- notes.php
+++ notes.php
@@ -17,22 +17,55 @@
     <title>Document</title>
     <style>
         #contain{
-            height:70vh;
-            overflow:scroll;
+            height:80vh;
+            overflow-y:scroll;
+            background-color:#3c3cf19a;
+        }
+        #notes{
+            border: 1px solid #3c3cf19a;
+            padding:10px;
+            border-radius:10px;
+            margin-bottom:20px;
+            margin-left:10px;
+            margin-right:10px;
+            background-color:black;
+            color:white;
+            margin-top:7px;
+            
+        }
+
+        *{
+            background-color:black;
+        }
+
+        #notes a{
+            text-decoration:none;
+            border-color: #3c3cf19a;
+            transition: 1s ease;
+            color:white;
+            background-color:#3c3cf19a;
+        }
+
+        #notes a:hover{
+            background-color:white;
+            color:#3c3cf19a;
         }
     </style>
 </head>
 <body>
-<?php include("TEMPLATE/header.html");?> 
-  <div id="contain" >
-    <div>
-        <?php foreach($data as $dt): ?>
-            <h1><?php echo $dt['title']?></h1>
-            <a href="note.php? id=<?php echo $dt['id']?>">more</a>
-        <?php endforeach?>
-        </div>   
-    </div>
+<div class="container">
+ <?php include("TEMPLATE/header.html");?> 
+  <div id="contain" class="">
+            <?php foreach($data as $dt): ?>
+                <div id="notes" class="">
+                           <h1><?php echo $dt['title']?></h1>
+                           <a href="note.php? id=<?php echo $dt['id']?>" class="btn">more</a>
+                </div>    
+            <?php endforeach?>
+     </div>
     <?php include("TEMPLATE/footer.html");?> 
+</div>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
